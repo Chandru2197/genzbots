d8d006ee6a2122e9af50550070925d996dffd140
Chandru2197 <55126754+Chandru2197@users.noreply.github.com>
2025-06-11 17:26:06 +0000

    new changes

--- src/pages/contactus.tsx
+++ src/pages/contactus.tsx
@@ -1,29 +1,56 @@
-import React, { useMemo } from "react";
-import Image from "next/image";
-// import ButtonPrimary from "./misc/ButtonPrimary";
-import {motion} from "framer-motion";
-import getScrollAnimation from "../utils/getScrollAnimation";
-import ScrollAnimationWrapper from "../components/layout/ScrollAnimationWrapper";
-import { Button } from "@mantine/core";
-import ContactSection from "@/components/ContactSection";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+// import React, { useMemo } from "react";
+// import Image from "next/image";
+// // import ButtonPrimary from "./misc/ButtonPrimary";
+// import {motion} from "framer-motion";
+// import getScrollAnimation from "../utils/getScrollAnimation";
+// import ScrollAnimationWrapper from "../components/layout/ScrollAnimationWrapper";
+// import { Button } from "@mantine/core";
+// import ContactSection from "@/components/ContactSection";
+// import Navbar from "@/components/Navbar";
+// import Footer from "@/components/Footer";
 
-const ContactUs = () => {
-  const scrollAnimation = useMemo(() => getScrollAnimation(), []);
+// const ContactUs = () => {
+//   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
 
+//   return (
+//     <div
+//       className="max-w-screen-xl mx-auto overflow-hidden"
+//       id="contact-us"
+//     >
+//       <Navbar/>
+//       <ScrollAnimationWrapper>
+//           <ContactSection/>
+//       </ScrollAnimationWrapper>
+//       <Footer/>
+//     </div>
+//   );
+// };
+
+// export default ContactUs;
+
+import type { Metadata } from 'next';
+
+import Contact2 from '../components/about_page/widgets/Contact2';
+import Features2 from '../components/about_page/widgets/Features2';
+import Hero from '../components/about_page/widgets/Hero';
+import { heroContact, contact2Contact, features2Contact } from '../shared/data/contact.data';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export const metadata: Metadata = {
+  title: 'Contact us',
+};
+
+const Page = () => {
   return (
-    <div
-      className="max-w-screen-xl mx-auto overflow-hidden"
-      id="contact-us"
-    >
-      <Navbar/>
-      <ScrollAnimationWrapper>
-          <ContactSection/>
-      </ScrollAnimationWrapper>
-      <Footer/>
-    </div>
+    <>
+      <Navbar />
+      <Hero {...heroContact} />
+      <Contact2 {...contact2Contact} />
+      <Features2 {...features2Contact} />
+      <Footer />
+    </>
   );
 };
 
-export default ContactUs;
+export default Page;
